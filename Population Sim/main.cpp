3e422c91db3c8da20478d91a3ba0be2eb9d9d373
Raj Joshi <geeky2307@gmail.com>
2020-07-16 11:55:15 -0400

    Initial Commit

---
+++ Population Sim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Population Sim
+//
+//  Created by Raj Joshi on 7/16/20.
+//  Copyright © 2020 Raj Joshi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
